bc4a4a92aeb8308542d2481fc6b493c1b1a6043e
“cheral” <khandediyacheral@gmail.com>
2020-07-09 22:01:08 +0530

    models

---
+++ models/order.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
+ 
+ 
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [CartItemSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    status: {
+      type: String,
+      default: "Not processed",
+      enum: ["Not processed", "Processing", "Shipped", "Delivered", "Cancelled"] // enum means string objects
+    },
+    updated: Date,
+    user: { type: ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
+ 
+module.exports = mongoose.model("Order", OrderSchema);
